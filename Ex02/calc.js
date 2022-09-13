f1971612d6625bc8afa408f941592eeecbec1be3
Claudio Henrique <claudio885henrique@gmail.com>
2022-09-12 21:59:36 -0300

    add ex02

---
+++ Ex02/calc.js
@@ -0,0 +1,8 @@
+var SomaFunc = require("./soma")
+var SubFunc = require("./sub")
+var DivFunc = require("./div")
+var MultiFunc = require("./multi")
+
+console.log(SubFunc(2,4))
+console.log(SomaFunc(10,4))
+console.log(MultiFunc(2,8))
